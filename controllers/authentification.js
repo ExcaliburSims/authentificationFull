acd733d3caa13b50bf0844202257a00e365430a5
Siméon zilu <50544380+ExcaliburSims@users.noreply.github.com>
2023-10-24 17:04:11 +0100

    feat: add login function

--- controllers/authentification.js
+++ controllers/authentification.js
@@ -2,11 +2,7 @@
 const {connect} = require('../model/index')
 
 exports.insert = (req, res) => {
-  // const { name, lastname, phone, mail, password } = req.body
-  // eslint-disable-next-line prettier/prettier
-  const {nom, prenom, email, telephone, sexe, password}= req.body
-  // const sqlInsert =
-  //   'INSERT INTO user (nom_user,prenom_user,tel_user,password) VALUES (?,?,?,?)'
+  const {nom, prenom, email, telephone, sexe, password} = req.body
   const sqlInsert =
     'INSERT INTO users(nom, prenom, email, telephone, sexe, password) VALUES (?,?,?,?,?,?)'
 
@@ -39,5 +35,28 @@ exports.users = (req, res) => {
   })
   connect.query('SELECT * FROM users', (err, result) => {
     res.send(result)
+  })
+}
+
+exports.login = (req, res) => {
+  const {nom, password} = req.body
+  const sqllogin = 'select * from users where nom=? AND password=?'
+
+  connect.getConnection(err => {
+    if (err) {
+      console.error(`Une erreur detectée : ${err}`)
+    } else {
+      console.log('Connecté à la base de données MySQL!')
+    }
+  })
+  connect.query(sqllogin, [nom, password], (err, result) => {
+    if (err) {
+      res.status(500).send(err)
+    } else if (result.length > 0) {
+      res.status(200).send(result[0])
+    } else {
+      res.status(400).send("L'utilisateur n'existe pas")
+    }
+    // connect.end()
   })
 }
